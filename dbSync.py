d08de04e9333a93e67d697d8f6c0084b35b39fb1
sebastian <callmeseb@gmail.com>
2016-03-27 21:56:17 +0200

    fourth commit

--- dbSync.py
+++ dbSync.py
@@ -9,26 +9,14 @@ def build_config_list(config_file):
         ip_address = config.get_ipaddr(pool_name)
         config_list.append([pool_name, mac_address, ip_address])
     return config_list
-print build_config_list('config.txt')
-
-def build_config_dict(config_file):
-    config = RouterDhcpConfig(config_file)
-    pool_name_list = config.get_dhcppoolname_list()
-    ip_mac_list = []
-    for pool_name in pool_name_list:
-        mac_address = config.get_macaddr(pool_name)
-        ip_address = config.get_ipaddr(pool_name)
-        ip_mac_list.append([mac_address, ip_address])
-    config_dict = {}
-    for i in range(len(pool_name_list)):
-        config_dict[pool_name_list[i]] = ip_mac_list[i]
-    return config_dict
-print build_config_dict('config.txt')
 
 config_list = build_config_list('config.txt')
 db = DatabaseHandler('rtrconfig.db')
-for item in config_list:
-    db.update(item)
+#db.create_baseline_table()
+#for item in config_list:
+#    db.sync_table_with_list(item)
+print db.get_free_ip("KOM-A")[0]
+print db.get_free_ip("KOM-B")[0]
 
 
 
--- helperfunctions.py
+++ helperfunctions.py
@@ -8,16 +8,46 @@ class DatabaseHandler():
     def __init__(self, db):
         self.conn = sqlite3.connect(db)
 
-    def update(self, list):
+    def create_baseline_table(self):
+        c = self.conn.cursor()
+        c.execute('''DROP TABLE IF EXISTS ip_to_mac;''')
+        c.execute('''CREATE TABLE ip_to_mac(
+            kom_type TEXT,
+            pool_name TEXT,
+            ip_address TEXT,
+            mac_address TEXT,
+            istaken TEXT);''')
+        self.conn.commit()
+        for j in range(0, 2):
+            if j == 0:
+                kom_type = "KOM-A"
+                octet = "0"
+            else:
+                kom_type = "KOM-B"
+                octet = "5"
+            for i in range(2, 240):
+                ip_address = "192.168." + str(octet) + "." + str(i)
+                c.execute('''
+                INSERT INTO ip_to_mac (kom_type, ip_address, istaken) VALUES (?, ?, ?)
+                ''', (kom_type, ip_address, "N"))
+        self.conn.commit()
+
+    def sync_table_with_list(self, list):
         self.list = list
         c = self.conn.cursor()
-        c.execute('''
-        UPDATE ip_to_mac
-        SET pool_name = ?, mac_address = ?, istaken = ?
-        WHERE ip_address = ?
+        c.execute(''' UPDATE ip_to_mac
+                    SET pool_name = ?, mac_address = ?, istaken = ?
+                    WHERE ip_address = ?
         ''', (self.list[0], self.list[1], "Y", self.list[2]))
         self.conn.commit()
 
+    def get_free_ip(self, kom_type):
+        c = self.conn.cursor()
+        c.execute(''' SELECT ip_address FROM ip_to_mac
+                    WHERE kom_type = ? AND istaken = "N"
+                    LIMIT 1''', (kom_type,))
+        free_ip = c.fetchone()
+        return free_ip
 
 class RouterDhcpConfig:
     def __init__(self, config_file):
